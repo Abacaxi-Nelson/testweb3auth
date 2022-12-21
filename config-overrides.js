6d38a604aa66380d3832bfe32d22a1940124c2ab
Nelson <nunes.nelson4@gmail.com>
2022-12-21 12:13:27 +0100

    this version maybe ?

--- config-overrides.js
+++ config-overrides.js
@@ -12,12 +12,6 @@ module.exports = function override(config) {
         url: require.resolve("url"),
     });
     config.resolve.fallback = fallback;
-    config.module.rules.push({
-        test: /\.m?js/,
-        resolve: {
-            fullySpecified: false
-        }
-    });
     config.plugins = (config.plugins || []).concat([
         new webpack.ProvidePlugin({
             process: "process/browser",
